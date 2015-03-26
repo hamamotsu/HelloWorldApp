a3da6349f1edb8720c3663789a1794e384d27c33
hamamotsu <hamamotsu@outlook.jp>
2015-03-26 14:41:24 +0900

    InitialCommit
    Windows8ストアアプリ開発入門Tutorial

---
+++ HelloWorldApp/DataModel/HelloWorldModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp.DataModel
+{
+    public class HelloWorldModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #region プロパティ
+
+        private string name;
+        /// <summary>
+        /// 名前を取得または設定します
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; NotifyPropertyChanged("Name"); }
+        }
+
+        private string message;
+        /// <summary>
+        /// メッセージを取得または設定します
+        /// </summary>
+        public string Message
+        {
+            get { return this.message; }
+            set { this.message = value; NotifyPropertyChanged("Message"); }
+        }
+
+        private string time = "朝";
+        /// <summary>
+        /// 時間帯を取得または設定します。
+        /// </summary>
+        public string Time
+        {
+            get { return this.time; }
+            set { this.time = value; NotifyPropertyChanged("Time"); }
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// 名前と時間帯から適切なメッセージを生成します。
+        /// </summary>
+        public void Greet()
+        {
+            // 出力メッセージのフォーマットを格納するための変数
+            string format = null;
+
+            // 選択項目に応じて出力メッセージのフォーマットを設定する。
+            switch (this.Time)
+            {
+                case "朝":
+                    format = "おはようございます{0}さん。";
+                    break;
+                case "昼":
+                    format = "こんにちは{0}さん。";
+                    break;
+                case "晩":
+                    format = "こんばんは{0}さん。";
+                    break;
+                default:
+                    throw new InvalidOperationException("不正な値");
+            }
+
+            // 出力メッセージをテキストブロックに設定する
+            this.Message = string.Format(format, this.Name);
+        }
+
+        private static HelloWorldModel defaultInstance;
+
+        /// <summary>
+        /// デフォルトのインスタンスを取得します。
+        /// </summary>
+        /// <returns></returns>
+        public static HelloWorldModel GetDefault()
+        {
+            if (defaultInstance == null)
+            {
+                defaultInstance = new HelloWorldModel();
+            }
+            return defaultInstance;
+        }
+    }
+}
